d69267584e5ce503d07fbc4e2cc3797ee9f60cf7
HuangJF <13600200870@163.com>
2024-06-04 20:12:37 +0800

    update

--- gateway.sql
+++ gateway.sql
@@ -21,36 +21,35 @@ CREATE TABLE gateway.roles (
 );
 
 CREATE TABLE gateway.menus (
-	id varchar(50) DEFAULT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring) NOT NULL,
+	id varchar(50) NOT NULL DEFAULT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),
 	"path" varchar(255) NOT NULL,
 	"name" varchar(255) NULL,
 	component varchar(255) NULL,
 	redirect varchar(255) NULL,
 	parent_id varchar(50) NULL,
-	status int4 DEFAULT 1 NULL,
+	status int4 NULL DEFAULT 1,
 	title varchar(255) NULL,
 	icon varchar(255) NULL,
-	always_show bool DEFAULT false NULL,
-	affix bool DEFAULT false NULL,
-	no_cache bool DEFAULT false NULL,
-	hidden bool DEFAULT false NULL,
-	can_to bool DEFAULT false NULL,
+	always_show bool NULL DEFAULT false,
+	affix bool NULL DEFAULT false,
+	no_cache bool NULL DEFAULT false,
+	hidden bool NULL DEFAULT false,
+	can_to bool NULL DEFAULT false,
 	active_menu varchar(255) NULL,
-	breadcrumb bool DEFAULT true NULL,
-	no_tags_view bool DEFAULT false NULL,
+	breadcrumb bool NULL DEFAULT true,
+	no_tags_view bool NULL DEFAULT false,
 	sort int4 NULL,
 	"type" int4 NULL,
-	create_time timestamp DEFAULT now() NULL,
+	create_time timestamp NULL DEFAULT now(),
 	update_time timestamp NULL,
 	CONSTRAINT menus_name_key UNIQUE (name),
 	CONSTRAINT menus_pkey PRIMARY KEY (id),
-	CONSTRAINT status_check CHECK ((status = ANY (ARRAY[0, 1]))),
-	CONSTRAINT menus_parent_id_fkey FOREIGN KEY (parent_id) REFERENCES gateway.menus(id) ON DELETE CASCADE
+	CONSTRAINT status_check CHECK ((status = ANY (ARRAY[0, 1])))
 );
 
 
 CREATE TABLE gateway.sys_log (
-	trance_id varchar(50) DEFAULT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring) NOT NULL,
+	trance_id varchar(50) NOT NULL DEFAULT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),
 	"source" varchar(255) NULL,
 	severity varchar(50) NULL,
 	oper_name varchar(255) NULL,
@@ -64,16 +63,16 @@ CREATE TABLE gateway.sys_log (
 	"result" text NULL,
 	start_time timestamp NULL,
 	end_time timestamp NULL,
-	create_time timestamp DEFAULT now() NULL,
+	create_time timestamp NULL DEFAULT now(),
 	update_time timestamp NULL,
 	CONSTRAINT sys_log_pkey PRIMARY KEY (trance_id)
 );
 
 CREATE TABLE gateway.user_role (
-	id varchar(50) DEFAULT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring) NOT NULL,
+	id varchar(50) NOT NULL DEFAULT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),
 	user_id varchar(50) NOT NULL,
 	role_id varchar(50) NOT NULL,
-	create_time timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	create_time timestamp NULL DEFAULT CURRENT_TIMESTAMP,
 	update_time timestamp NULL,
 	CONSTRAINT user_role_user_id_role_id_key UNIQUE (user_id, role_id),
 	CONSTRAINT user_role_role_id_fkey FOREIGN KEY (role_id) REFERENCES gateway.roles(id),
@@ -81,13 +80,12 @@ CREATE TABLE gateway.user_role (
 );
 
 CREATE TABLE gateway.role_menu (
-	id varchar(50) DEFAULT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring) NOT NULL,
+	id varchar(50) NOT NULL DEFAULT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),
 	role_id varchar(50) NOT NULL,
 	menu_id varchar(50) NOT NULL,
-	create_time timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	create_time timestamp NULL DEFAULT CURRENT_TIMESTAMP,
 	update_time timestamp NULL,
 	CONSTRAINT role_menu_role_id_menu_id_key UNIQUE (role_id, menu_id),
 	CONSTRAINT role_menu_menu_id_fkey FOREIGN KEY (menu_id) REFERENCES gateway.menus(id),
 	CONSTRAINT role_menu_role_id_fkey FOREIGN KEY (role_id) REFERENCES gateway.roles(id)
 );
-
